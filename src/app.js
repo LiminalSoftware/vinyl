6d36145ca0a354e916a5b4f5d5c3bcac15dfd096
Bryan White <bryanchriswhite@gmail.com>
2015-12-21 00:44:53 -0600

    prep for demo deploy:
    * translate heads-up text into german
    * replace 6/9 samples with actual tracks
    * inline down-arrow svg

--- src/app.js
+++ src/app.js
@@ -35,18 +35,12 @@ const qs = document.querySelector.bind(document)
   , scrubberDefaultY = 6
   , scrubberDefaultX = 0
   , songList = {
-  0: {index: 0, id: 'bh', title: 'Bohemian Rhapsody', file: 'songs/bohemian-rhapsody.mp3', duration: '05:53'},
-  1: {index: 1, id: 'wwry', title: 'We Will Rock You', file: 'songs/we-will-rock-you.mp3', duration: '02:03'},
-  2: {index: 2, id: 'kq', title: 'Killer Queen', file: 'songs/killer-queen.mp3', duration: '03:01'},
-  3: {index: 3, id: 'watc', title: 'We Are the Champions', file: 'songs/we-are-the-champions.mp3', duration: '03:03'},
-  4: {
-    index: 4,
-    id: 'aobtd',
-    title: 'Another One Bites the Dust',
-    file: 'songs/another-one-bites-the-dust.mp3',
-    duration: '03:37'
-  },
-  5: {index: 5, id: 'ssor', title: 'Seven Seas Of Rhye', file: 'songs/seven-seas-of-rhye.mp3', duration: '02:49'},
+  0: {index: 0, id: 'nu-disco', title: 'Nu Disco', file: 'mixes/nu-disco.mp3', duration: '04:12'},
+  1: {index: 1, id: 'cafe-del-mar', title: 'Cafe Del Mar', file: 'mixes/cafe-del-mar.mp3', duration: '05:41'},
+  2: {index: 2, id: 'cosy-chill', title: 'Cosy Chill', file: 'mixes/cosy-chill.mp3', duration: '04:07'},
+  3: {index: 3, id: 'chillout-dreams', title: 'Chillout Dreams', file: 'mixes/chillout-dreams.mp3', duration: '07:33'},
+  4: { index: 4, id: 'club-hits-1', title: 'Club Hits 1', file: 'mixes/club-hits-1.mp3', duration: '05:03' },
+  5: {index: 5, id: 'monster-beat', title: 'Monster Beat', file: 'mixes/monster-beat.mp3', duration: '05:34'},
   6: {index: 6, id: 'loml', title: 'Love Of My Life', file: 'songs/love-of-my-life.mp3', duration: '03:38'},
   7: {index: 7, id: 'ptg', title: 'Play The Game', file: 'songs/play-the-game.mp3', duration: '03:32'},
   8: {index: 8, id: 'tsmgo', title: 'The Show Must Go On', file: 'songs/the-show-must-go-on.mp3', duration: '04:32'}
